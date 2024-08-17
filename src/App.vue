26341d46fb3d7313703023630abadd6e0c9f0015
johnyatesiv <john.yatesiv@gmail.com>
2024-08-16 20:00:36 -0700

    style

--- src/App.vue
+++ src/App.vue
@@ -4,18 +4,18 @@
 <template>
   <div class="sparkle-background"></div>
   <div className="container">
-    <div className="title fade-out">Hello out there</div>
+    <!-- <div className="title fade-out">Hello out there</div> -->
     <div className="content fade-in">
-      <p>$ I'm John, a <span className="role rotate">software developer<span className="blinking">_</span></span></p>
+      <p>$ Hello there. I'm John, a <span className="role rotate">software developer<span className="blinking">_</span></span></p>
       <div className="portrait"></div>
     </div>    
-    <div className="content slow-fade-in">
+    <div className="content">
       Check out what I'm up to      
       <div className="links">
         <a className="link" href="https://github.com/johnyatesiv" target="blank" rel="noopener noreferrer">
           <v-icon name="px-git-commit" scale="2" /> Code
         </a>
-        <a className="link" href="/resume.pdf" target="blank" rel="noopener noreferrer">
+        <a className="link" href="#resume-section">
           <v-icon name="hi-document-download" scale="2" /> Resume
         </a>
         <a className="link" href="mailto:john.yatesiv@gmail.com?subject=Let's chat" target="blank" rel="noopener noreferrer">
@@ -32,13 +32,62 @@
       </div>
     </div>    
     <div className="amuse-bouche"></div>
-  </div>
-  <div className="footer slow-fade-in">
+    <section id="resume-section" class="section">
+      <div class="resume-job">
+        <h2>2021 - Present: BallerTV</h2>
+        <ul>
+          <li>Lead developer for operations and scalability team</li>
+          <li>Leader in steering committees improving developer experience, DevOps and test automation</li>
+          <li>Provided on-call support to operations team executing content production nationwide</li>
+          <li>Improved tech stack leading to improved developer experience and faster feature turnaround</li>
+        </ul>
+      </div>
+      <div class="resume-job">
+        <h2>2019 - 2021: Alpha Ori</h2>
+        <ul>
+          <li>Lead development for onboard and cloud enterprise maritime analysis systems</li>
+          <li>Trained teams in test-driven methodologies</li>
+          <li>Collaborated with DevOps team on reducing product install time by days</li>
+          <li>Lead and mentored cross-cultural teams in multiple time zones</li>
+        </ul>
+      </div>
+      <div class="resume-job">
+        <h2>2016 - 2019: Tech Lead @ Driven Deliveries</h2>
+        <ul>
+          <li>Lead technology team at a publicly traded company (merged with OTC:STMH)</li>
+          <li>Created ecommerce sites, delivery logistics software and shared microservices</li>
+          <li>Audited regulatory compliance for the company's brand portfolio</li>
+          <li>Reported on development activities and strategy to the board of directors</li>
+        </ul>
+      </div>
+      <div class="resume-job">
+        <h2>2012 - 2016: Tech Lead @ Locbit</h2>
+        <ul>
+          <li>
+              Architected and implemented a scalable product capable of protocol agnostic
+              building automation control using the MEAN stack
+          </li>
+          <li>
+            Led technical analysis and development of a commercial IoT solution in conjunction with Panasonic CPBD
+          </li>
+          <li>
+            Z-wave, ZigBee and DECT ULE network design and testing
+          </li>
+        </ul>
+      </div>
+      <div>
+        <a href="/resume.pdf" target="blank" rel="noopener noreferrer">
+          Download a copy
+        </a>
+      </div>
+    </section>
+    <div className="footer">
       <p><a href="https://creativecommons.org/licenses/by-nc/4.0/">
         <v-icon name="fa-creative-commons"></v-icon> BY-NC</a>
         2024
       </p>
       <p>Site designed with Vue + Vite</p>
+  </div>
   </div>
 </template>
 
@@ -72,8 +121,8 @@
 
 @keyframes fadeIn {
   from {
-    opacity: 0;
-    font-size: 1rem;
+    opacity: 0.2;
+    font-size: 1.4rem;
   }
   to {
     opacity: 1;
@@ -153,6 +202,7 @@
 .container {
   height: 100vh;
   display: flex;
+  flex-direction: column;
   align-items: center;
 }
 
@@ -172,8 +222,8 @@
   opacity: 0;
   animation-name: fadeIn;
   animation-timing-function: ease;
-  animation-delay: 2s;
-  animation-duration: 4s;
+  /* animation-delay: 2s; */
+  animation-duration: 3s;
   animation-fill-mode: forwards;
 }
 
